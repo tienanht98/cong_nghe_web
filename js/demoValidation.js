5cba26698c89579c49f293d656a5b829874cf424
anh-tt@trente.asia <anh-tt@trente.asia>
2019-03-05 11:20:30 +0700

    first commit

---
+++ js/demoValidation.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    $(".myform").validate({
+        onfocusout: false,
+        onkeyup: false,
+        onclick: false,
+        rules: {
+            "name":{
+                required : true,
+
+            },
+            "username": {
+                required: true,
+                maxlength: 15
+            },
+            "password": {
+                required: true,
+                minlength: 8
+            },
+            "re-password": {
+                equalTo: "#password",
+                minlength: 8
+
+            },
+            "bday": {
+                required :true,
+            }
+        },
+        messages: {
+            "name": {
+                required: "Bắt buộc nhập name",
+                maxlength: "Hãy nhập tối đa 15 ký tự"
+            },
+            "username": {
+                required: "Bắt buộc nhập username",
+                maxlength: "Hãy nhập tối đa 15 ký tự"
+            },
+            "password": {
+                required: "Bắt buộc nhập password",
+                minlength: "Hãy nhập ít nhất 8 ký tự"
+            },
+            "re-password": {
+                equalTo: "Hai password phải giống nhau",
+                minlength: "Hãy nhập ít nhất 8 ký tự"
+            }
+        }
+    });
+});
